3f34d16cf8524d8af3c441c86c3956605011bfb7
V S Vishwas <148931448+v-s-v-i-s-h-w-a-s@users.noreply.github.com>
2025-07-24 00:11:07 +0530

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,9 +1,8 @@
 from fastapi.testclient import TestClient
-from app.main import app
+from app.main import app  # <- this needs the PYTHONPATH fix
 
 client = TestClient(app)
 
-def test_root():
-    response = client.get("/")
+def test_health():
+    response = client.get("/health")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
